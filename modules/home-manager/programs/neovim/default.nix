da2544f3a62bca43c01b93947ae7ab268eb8315e
Civi <civi@rcg.fun>
2026-03-04 00:30:15 +0800

    feat: add tree-sitter parsers

--- modules/home-manager/programs/neovim/default.nix
+++ modules/home-manager/programs/neovim/default.nix
@@ -11,7 +11,43 @@
     defaultEditor = true;
     plugins = with pkgs.vimPlugins; [
       neorg
-      nvim-treesitter
+      (nvim-treesitter.withPlugins (p: [
+        p.bash
+        p.css
+        p.diff
+        p.dockerfile
+        p.editorconfig
+        p.fish
+        p.git_config
+        p.git_rebase
+        p.gitattributes
+        p.gitcommit
+        p.gitignore
+        p.go
+        p.html
+        p.javascript
+        p.json
+        p.latex
+        p.lua
+        p.luadoc
+        p.nginx
+        p.nix
+        p.python
+        p.regex
+        p.ron
+        p.rust
+        p.scss
+        p.ssh_config
+        p.svelte
+        p.toml
+        p.tsx
+        p.typescript
+        p.typst
+        p.vue
+        p.yaml
+        p.zig
+        p.zsh
+      ]))
     ];
     extraPackages = with pkgs; [
       fish-lsp
