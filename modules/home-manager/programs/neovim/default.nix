9f205b04c4ce8ea1c2187326849dd191d427a416
Civi <19486398+angribot@users.noreply.github.com>
2026-03-12 13:48:02 +0800

    refactor: seprate plugin and package files

--- modules/home-manager/programs/neovim/default.nix
+++ modules/home-manager/programs/neovim/default.nix
@@ -2,80 +2,13 @@
 {
   programs.neovim = {
     enable = true;
-    # package = pkgs.neovim;
     viAlias = true;
     vimAlias = true;
     withNodeJs = false;
     withPython3 = false;
     withRuby = false;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [
-      everforest
-      snacks-nvim
-      mini-nvim
-      nvim-autopairs
-      conform-nvim
-      fidget-nvim
-      flash-nvim
-      neorg
-      nvim-lspconfig
-      (nvim-treesitter.withPlugins (p: [
-        p.bash
-        p.css
-        p.diff
-        p.dockerfile
-        p.editorconfig
-        p.fish
-        p.git_config
-        p.git_rebase
-        p.gitattributes
-        p.gitcommit
-        p.gitignore
-        p.go
-        p.html
-        p.javascript
-        p.json
-        p.latex
-        p.lua
-        p.luadoc
-        p.nginx
-        p.nix
-        p.python
-        p.regex
-        p.ron
-        p.rust
-        p.scss
-        p.ssh_config
-        p.svelte
-        p.toml
-        p.tsx
-        p.typescript
-        p.typst
-        p.vue
-        p.yaml
-        p.zig
-        p.zsh
-      ]))
-    ];
-    extraPackages = with pkgs; [
-      emmylua-ls
-      fish-lsp
-      nixd
-      nixfmt
-      nodePackages.bash-language-server
-      oxfmt
-      oxlint
-      shellcheck
-      shfmt
-      stylua
-      tombi
-      vscode-langservers-extracted
-      yaml-language-server
-      # Snacks.image
-      ghostscript
-      imagemagick
-      mermaid-cli
-      tectonic
-    ];
+    plugins = import ./plugins.nix pkgs;
+    extraPackages = import ./packages.nix pkgs;
   };
 }
