d39616849a10cee4626afda736d97fb7e51b8a21
Civi <civi@rcg.fun>
2026-03-12 13:48:02 +0800

    refactor: seprate plugin and package files

---
+++ modules/home-manager/programs/neovim/packages.nix
@@ -0,0 +1,20 @@
+pkgs: with pkgs; [
+  emmylua-ls
+  fish-lsp
+  nixd
+  nixfmt
+  nodePackages.bash-language-server
+  oxfmt
+  oxlint
+  shellcheck
+  shfmt
+  stylua
+  tombi
+  vscode-langservers-extracted
+  yaml-language-server
+  # Snacks.image
+  ghostscript
+  imagemagick
+  mermaid-cli
+  tectonic
+]
