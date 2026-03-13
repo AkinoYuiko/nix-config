5a7ade01168780645d8786d3bef713b6abc5543d
Civi <civi@rcg.fun>
2026-03-13 21:44:31 +0800

    feat: enable homebrew auto update

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -2,11 +2,13 @@
 {
   homebrew = {
     enable = true;
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
     brews = [
       "ddns-go"
       # "git-flow"
       "nginx"
     ];
-    casks = [];
+    casks = [ ];
   };
 }
