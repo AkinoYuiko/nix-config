cec9f263b811bada32a5f9d87ad43dca19feb222
Civi <19486398+angribot@users.noreply.github.com>
2026-06-24 11:13:04 +0800

    fix(neovim): restore jsonls on node 24

--- modules/home-manager/programs/neovim/packages.nix
+++ modules/home-manager/programs/neovim/packages.nix
@@ -1,4 +1,12 @@
-pkgs: with pkgs; [
+pkgs:
+let
+  vscode-json-language-server-node24 = pkgs.writeShellScriptBin "vscode-json-language-server" ''
+    cd "${pkgs.vscode-langservers-extracted}/lib/node_modules/vscode-langservers-extracted"
+    exec ${pkgs.nodejs_24}/bin/node --input-type=commonjs -e 'process.argv = [process.argv[0], "vscode-json-language-server"].concat(process.argv.slice(1)); require("./lib/json-language-server/node/jsonServerMain.js")' -- "$@"
+  '';
+in
+with pkgs;
+[
   # emmylua-ls
   lua-language-server
   fish-lsp
@@ -11,6 +19,7 @@ pkgs: with pkgs; [
   shfmt
   stylua
   tombi
+  vscode-json-language-server-node24
   vscode-langservers-extracted
   yaml-language-server
   # Snacks.image
