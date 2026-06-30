10eac3d1bae62dd88ce923e27f68ac46e6273d8f
Civi <19486398+angribot@users.noreply.github.com>
2026-06-30 23:40:13 +0800

    fix(neovim): restore jsonls startup
    Use the packaged vscode-langservers-extracted JSON language server instead of a custom node24 wrapper whose internal path no longer matches nixpkgs.
    
    Keep jsonls scoped to Neovim by leaving it in programs.neovim.extraPackages only.

--- modules/home-manager/programs/neovim/packages.nix
+++ modules/home-manager/programs/neovim/packages.nix
@@ -1,10 +1,4 @@
 pkgs:
-let
-  vscode-json-language-server-node24 = pkgs.writeShellScriptBin "vscode-json-language-server" ''
-    cd "${pkgs.vscode-langservers-extracted}/lib/node_modules/vscode-langservers-extracted"
-    exec ${pkgs.nodejs_24}/bin/node --input-type=commonjs -e 'process.argv = [process.argv[0], "vscode-json-language-server"].concat(process.argv.slice(1)); require("./lib/json-language-server/node/jsonServerMain.js")' -- "$@"
-  '';
-in
 with pkgs;
 [
   # emmylua-ls
@@ -18,7 +12,6 @@ with pkgs;
   shfmt
   # stylua # remove due to cargo exists
   tombi
-  vscode-json-language-server-node24
   vscode-langservers-extracted
   yaml-language-server
   # Snacks.image
