de6875d91d46138645dd6ec75e9c37f4776dac89
Civi <19486398+angribot@users.noreply.github.com>
2026-08-07 09:14:54 +0800

    feat(pi): add oxfmt and oxlint to pi bin

--- modules/home-manager/programs/pi/default.nix
+++ modules/home-manager/programs/pi/default.nix
@@ -6,5 +6,7 @@
     ".pi/agent/bin/rg".source = "${pkgs.ripgrep}/bin/rg";
     ".pi/agent/bin/shellcheck".source = "${pkgs.shellcheck}/bin/shellcheck";
     ".pi/agent/bin/actionlint".source = "${pkgs.actionlint}/bin/actionlint";
+    ".pi/agent/bin/oxfmt".source = "${pkgs.oxfmt}/bin/oxfmt";
+    ".pi/agent/bin/oxlint".source = "${pkgs.oxlint}/bin/oxlint";
   };
 }
